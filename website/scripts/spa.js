aad632fb895b389dd35e561cfa8d1afa13197eb5
borecjeborec1 <robingrundel@seznam.cz>
2023-01-16 21:31:39 +0100

    todo

--- website/scripts/spa.js
+++ website/scripts/spa.js
@@ -25,10 +25,3 @@ window.onhashchange = handleLocation;
 handleLocation()
 
 
-function displayDocs(currentElement) {
-  let lastElement = currentElement.parentElement.querySelector(".selected")
-  lastElement.classList.remove("selected")
-  document.getElementById(lastElement.dataset.path).style.display = "none"
-  document.getElementById(currentElement.dataset.path).style.display = "block"
-  currentElement.classList.add("selected")
-}
