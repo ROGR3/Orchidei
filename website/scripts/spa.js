e78d25f33f1b83bd0b45df515751d7e57aac4e0c
borecjeborec1 <robingrundel@seznam.cz>
2023-01-16 21:30:04 +0100

    website

---
+++ website/scripts/spa.js
@@ -0,0 +1,34 @@
+
+const routes = {
+  404: "/views/404.html",
+  home: "/views/home.html",
+  docs: "/views/docs.html",
+  downloads: "/views/downloads.html",
+};
+async function handleLocation() {
+  let lis = document.querySelectorAll(".navli")
+
+  let path = window.location.hash.replace("#", "") || "home";
+  let route = routes[path] || routes[404];
+  let html = await fetch(route).then((data) => data.text());
+  document.getElementById("container").innerHTML = html;
+  lis.forEach(li => {
+    li.classList.remove("selected")
+    if (li.firstElementChild.href.includes(path)) {
+      li.classList.add("selected")
+    }
+  })
+};
+
+window.onhashchange = handleLocation;
+
+handleLocation()
+
+
+function displayDocs(currentElement) {
+  let lastElement = currentElement.parentElement.querySelector(".selected")
+  lastElement.classList.remove("selected")
+  document.getElementById(lastElement.dataset.path).style.display = "none"
+  document.getElementById(currentElement.dataset.path).style.display = "block"
+  currentElement.classList.add("selected")
+}
