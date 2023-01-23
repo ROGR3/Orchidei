56af8a9c113825c9156e24aa2ef28336a4e4e737
borecjeborec1 <robingrundel@seznam.cz>
2023-01-23 15:01:56 +0100

    server

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -5,3 +5,16 @@ function displayDocs(currentElement) {
   document.getElementById(currentElement.dataset.path).style.display = "block"
   currentElement.classList.add("selected")
 }
+
+function downloadWindows() {
+  addToDownloads()
+  var link = document.createElement("a");
+  // link.download = name;
+  link.href = "http://127.0.0.1:5502/index.html#downloads";
+  link.click();
+}
+
+async function addToDownloads() {
+  const response = await fetch("http://localhost:3000/download-info/").then(res => res.json())
+  console.log(response)
+}
